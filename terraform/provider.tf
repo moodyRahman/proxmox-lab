389a79001c91b7a6022015b1c32e9aecb683a6b0
Moody Rahman <moody.byte@gmail.com>
2025-10-29 22:10:19 -0400

    state files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.86.0" # x-release-please-version
+    }
+  }
+}
+
+
 provider "proxmox" {
   endpoint = "https://ermes:8006"
   insecure = true
