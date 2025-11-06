231516f09822097fe317ea7cca5269beb7c097ff
Moody Rahman <moududur921@gmail.com>
2025-11-06 17:31:47 -0500

    provision some vm's

--- terraform/modules/ephemeral/main.tf
+++ terraform/modules/ephemeral/main.tf
@@ -13,6 +13,34 @@ resource "proxmox_virtual_environment_vm" "jumpbox" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "worker-1" {
+  name      = "worker-1"
+  node_name = var.node-name
+  vm_id = 201
+
+  clone {
+    vm_id = var.base_vm_id
+  }
+
+  agent {
+    enabled = true
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "worker-2" {
+  name      = "worker-2"
+  node_name = var.node-name
+  vm_id = 202
+
+  clone {
+    vm_id = var.base_vm_id
+  }
+
+  agent {
+    enabled = true
+  }
+}
+
 terraform {
   required_providers {
     proxmox = {
