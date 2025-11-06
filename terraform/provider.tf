bc7a1ddbdcca9116910f30051d101f0012de8b6b
Moody Rahman <moududur921@gmail.com>
2025-11-05 20:02:48 -0500

    adjusted host to resolve ts magicdns and fixed endpoint

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "proxmox" {
-  endpoint = "https://ermes:8006"
+  endpoint = "https://ermes.tail5b1a8.ts.net:8006"
   insecure = true
   username = var.proxmox-username
   password = var.proxmox-password
