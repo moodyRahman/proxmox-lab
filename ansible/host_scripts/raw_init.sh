ec8fe2d4838541f8d17f6b008d236de5a5b5e920
Moody Rahman <moududur921@gmail.com>
2025-11-07 22:38:49 -0500

    adjust raw_init to passwordless root

--- ansible/host_scripts/raw_init.sh
+++ ansible/host_scripts/raw_init.sh
@@ -1,3 +1,4 @@
 
-apt install sudo cloud-init
+apt install sudo cloud-init python
 usermod -aG sudo moody
+echo "moody ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers >/dev/null && sudo visudo -cf /etc/sudoers
