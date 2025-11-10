35e2ecf11c535ff94cbbfa3e88342683b403e5ff
Moody Rahman <moududur921@gmail.com>
2025-11-09 22:45:36 -0500

    added persistent reference to template

--- terraform/modules/persistent/main.tf
+++ terraform/modules/persistent/main.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_vm" "base_template" {
-  name      = "base-image"
+  name      = "base-image-old-part"
   node_name = var.node-name
-  vm_id = 102
+  vm_id = 106
   template = true
   
   lifecycle {
@@ -10,10 +10,26 @@ resource "proxmox_virtual_environment_vm" "base_template" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "base_template_raw" {
+  name      = "base-image-raw"
+  node_name = var.node-name
+  vm_id = 104
+  template = true
+  
+  lifecycle {
+    ignore_changes  = all
+    prevent_destroy = true
+  }
+}
+
+
 output "base_template_vm_id" {
   value = proxmox_virtual_environment_vm.base_template.vm_id
 }
 
+output "base_template_raw_vm_id" {
+  value = proxmox_virtual_environment_vm.base_template_raw.vm_id
+}
 
 terraform {
   required_providers {
